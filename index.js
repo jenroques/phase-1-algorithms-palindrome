8c2a6473e1572f7279a04c96dbc9cf6432555040
Jennifer Roques <jenniferroques@gmail.com>
2021-12-28 10:54:09 -0600

    commit tree ccab37135509819d7dcac1aa7f702dbbf4615689

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  if (word.length <= 1) {
+    return true
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      return isPalindrome(word.slice(1, -1))
+    } else {
+      return false
+    }
+  }
 }
 
+
+isPalindrome("abba");
+
+
 /* 
-  Add your pseudocode here
+  Declare Function 
+   if length is less than 1 return true
+
+   else
+   if word 0 is strictly equal to the end (-1) 
+   slice out 1 and -1 
+   
+   else 
+   return false
 */
 
 /*
-  Add written explanation of your solution here
+  If the word is one letter or less, return true 
+  Else deconstruct the word using splice to compare letters
+
+  If the letters are not strictly equal return false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
